f86632935d90378714e9ad36d70bd15d8e5fca46
joel <joel@insector.se>
2015-02-05 17:29:21 +0100

    dev

--- node/model.py
+++ node/model.py
@@ -1,44 +1,29 @@
 # -*- coding: utf-8 -*-
 import datetime
 
-from sqlalchemy import Column, Integer, Unicode, ForeignKey, DateTime, and_, UnicodeText, PickleType, or_
-from sqlalchemy.orm import relationship, EXT_CONTINUE
-from sqlalchemy.orm.interfaces import MapperExtension
+from sqlalchemy import Column, Integer, Unicode, ForeignKey, DateTime, and_, UnicodeText, or_
+from sqlalchemy.orm import relationship
 from sqlalchemy.orm.session import object_session
 from sqlalchemy.ext.mutable import MutableDict
 
 from node import Base
-from node.util import get_discriminators
+from node.util import get_discriminators, JSONEncodedObj
 
-class Callback(MapperExtension):
-    """ Extention to add pre-commit hooks.
-
-    Hooks will be called in Mapped classes if they define any of these
-    methods:
-      * _pre_insert()
-      * _post_insert()
-      * _pre_delete()
-      * _pre_update()
-    """
-    def before_insert(self, mapper, connection, instance):
-        f = getattr(instance, "_pre_insert", None)
-        if f: f()
-        return EXT_CONTINUE
-
-    def after_insert(self, mapper, connection, instance):
-        f = getattr(instance, "_post_insert", None)
-        if f: f()
-        return EXT_CONTINUE
-
-    def before_delete(self, mapper, connection, instance):
-        f = getattr(instance, "_pre_delete", None)
-        if f: f()
-        return EXT_CONTINUE
-
-    def before_update(self, mapper, connection, instance):
-        f = getattr(instance, "_pre_update", None)
-        if f: f()
-        return EXT_CONTINUE
+# class Foo(object):
+#     def __getattr__(self, name):
+#         if some_predicate(name):
+#             # ...
+#         else:
+#             # Default behaviour
+#             raise AttributeError
+# However, unlike __getattr__, __getattribute__ will be called first (only works for new style classes i.e. those that inherit from object). In this case, you can preserve default behaviour like so:
+# class Foo(object):
+#     def __getattribute__(self, name):
+#         if some_predicate(name):
+#             # ...
+#         else:
+#             # Default behaviour
+#             return object.__getattribute__(self, name)
 
 
 class Edge(Base):
@@ -47,46 +32,67 @@ class Edge(Base):
 
     __tablename__ = "edges"
     id = Column(Integer, primary_key=True)
-    edge_key = Column(Unicode(100), unique=True)
-    name = Column(Unicode(255))
-    group_name = Column(Unicode(100))
-    relation_type = Column(Unicode(100))
-    index = Column(Integer)
-    meta_data = Column(MutableDict.as_mutable(PickleType))
+    _edge_key = Column('edge_key', Unicode(100), unique=True)
+    _name = Column('name', Unicode(255))
+    _group_name = Column('group_name', Unicode(100))
+    _relation_type = Column('relation_type', Unicode(100))
+    _index = Column('index', Integer)
+    _meta_data = Column('meta_data', MutableDict.as_mutable(JSONEncodedObj))
 
     left_id = Column(Integer, ForeignKey('nodes.id'))
-    parent = relationship(  "Node",
-                            backref="children_relations",
-                            primaryjoin="Edge.left_id==Node.id")
+    parent = relationship("Node",
+                          backref="children_relations",
+                          primaryjoin="Edge.left_id==Node.id")
 
     right_id = Column(Integer, ForeignKey('nodes.id'))
-    child = relationship(   "Node",
-                            backref="parent_relations",
-                            primaryjoin="Edge.right_id==Node.id",
-                            lazy='joined')
+    child = relationship("Node",
+                         backref="parent_relations",
+                         primaryjoin="Edge.right_id==Node.id",
+                         lazy='joined')
 
     def __init__(self, *args, **kw):
         super(Edge, self).__init__(*args, **kw)
+
+
+    # def __getattribute__(self, name):
+    #     attr = '_{0}'.format(name)
+    #     if hasattr(self, attr):
+    #         return super(Edge, self).__getattribute__(attr)
+    #     return super(Edge, self).__getattribute__(name)
+
+    # def __getattr__(self, name):
+    #     # look for _attribute
+    #     attr = '_{0}'.format(name)
+    #     if hasattr(self, attr):
+    #         object.__getattr__(self, name)
+    #         # return super(Edge, self).__getattr__(attr)
+
+    # def __setattr__(self, name, value):
+    #     # look for _attribute
+    #     attr = '_{0}'.format(name)
+    #     if hasattr(self, attr):
+    #         object.__setattr__(self, name, value)
+    #         # super(Edge, self).__setattr__(attr, value)
 
     @property
     def session(self):
         return object_session(self)
 
     def _set_metadata_value(self, key, value):
-        if not self.meta_data:
-            self.meta_data = {}
+        if not self._meta_data:
+            self._meta_data = {}
         # Remove keys with value None
-        if value == None:
+        if value is None:
             try:
-                del(self.meta_data[key])
+                del(self._meta_data[key])
             except Exception:
                 pass
         else:
-            self.meta_data[key] = value
+            self._meta_data[key] = value
 
         # remove dict
-        if len(self.meta_data.keys()) == 0:
-            self.meta_data = None
+        if len(self._meta_data.keys()) == 0:
+            self._meta_data = None
 
     # ==================
     # = STATIC METHODS =
@@ -100,96 +106,112 @@ class Edge(Base):
         # recursive find parent as child or grandchild
         if parent and child:
             for grandchild in child.children:
-                Edge.check_circular_reference(parent,grandchild)
+                Edge.check_circular_reference(parent, grandchild)
 
     @staticmethod
     def create_edge(parent, child, group=None, relation_type=None, metadata=None):
         if isinstance(parent, int):
-            parent = Node.get(parent)
+            parent = AbstractNode.get(parent)
         if isinstance(child, int):
-            child = Node.get(child)
-        Edge.check_circular_reference(parent,child) # raises error if fail
+            child = AbstractNode.get(child)
+        Edge.check_circular_reference(parent, child)  # raises error if fail
         edge = Edge(metadata=metadata)
         edge.parent = parent
         edge.child = child
-        edge.group_name = group
-        edge.relation_type = relation_type
+        edge._group_name = group
+        edge._relation_type = relation_type
         return edge
 
     @staticmethod
     def update_child_edges(parent, children, **kws):
         """Update parent's child edges. child_ids=[list of children], discriminators=[list of discriminators], group="name of edge group", metadata=[list of dicts] """
-        Edge.update_related_edges( parent, children, Edge.CHILD, **kws )
+        Edge.update_related_edges(parent, children, Edge.CHILD, **kws)
 
     @staticmethod
     def update_parent_edges(child, parents, **kws):
         """Update child's parent edges. parents=[list of parents], discriminators=[list of discriminators], group="name of edge group", metadata=[list of dicts] """
-        Edge.update_related_edges( child, parents, Edge.PARENT, **kws )
+        Edge.update_related_edges(child, parents, Edge.PARENT, **kws)
 
     @staticmethod
     def update_related_edges(node, related_nodes, relation=CHILD, discriminators=None, group=None, relation_type=None, metadata=None):
-        """Update node's related parent or child edges. related_nodes=[list of related nodes], discriminators=[list of discriminators], group="name of edge group", relation_type="type of relation", metadata=[list of dicts] """
+        """Update node's related parent or child edges. related_nodes=[list of related nodes],
+        discriminators=[list of discriminators], group="name of edge group", relation_type="type of relation",
+        metadata=[list of dicts] """
         assert isinstance(related_nodes, list)
-        clauses = (Edge.parent==node, ) if relation == Edge.CHILD else (Edge.child==node, )
+        clauses = (Edge.parent == node, ) if relation == Edge.CHILD else (Edge.child == node, )
 
-        if not group == False:
-            clauses = clauses + (Edge.group_name==group, )
-        if not relation_type == False:
-            clauses = clauses + (Edge.relation_type==relation_type, )
+        if group is not False:
+            clauses = clauses + (Edge._group_name == group, )
+        if relation_type is not False:
+            clauses = clauses + (Edge._relation_type == relation_type, )
         if discriminators:
-            clauses = clauses + (Node.discriminator.in_(discriminators), )
+            clauses = clauses + (AbstractNode.discriminator.in_(discriminators), )
 
         # iterate existing edges
         s = node.session
         existing_edges = s.query(Edge).join(Edge.child if relation == Edge.CHILD else Edge.parent).filter(and_(*clauses)).all()
         for edge in existing_edges:
-            related_node = edge.child if relation==Edge.CHILD else edge.parent
+            related_node = edge.child if relation == Edge.CHILD else edge.parent
             if related_node in related_nodes:
-                i = related_nodes.index(related_node) # index in list
+                i = related_nodes.index(related_node)  # index in list
                 if metadata:
                     if i < len(metadata):
-                        edge.meta_data = metadata[i] # set metadata on edge
-                    metadata = metadata[:i] + metadata[i+1:] # remove index
-                related_nodes = related_nodes[:i] + related_nodes[i+1:] # remove same index
+                        edge.meta_data = metadata[i]  # set metadata on edge
+                    metadata = metadata[:i] + metadata[i + 1:]  # remove index
+                related_nodes = related_nodes[:i] + related_nodes[i + 1:]  # remove same index
             else:
                 s.delete(edge)
         # iterate non existing nodes and create new edges
         for i, related_node in enumerate(related_nodes):
             md = metadata[i] if metadata and i < len(metadata) else None
             if relation == Edge.CHILD:
-                new_edge = Edge.create_edge( node, related_node, group=group, metadata=md )
+                new_edge = Edge.create_edge(node, related_node, group=group, metadata=md)
             else:
-                new_edge = Edge.create_edge( related_node, node, group=group, metadata=md )
-            s.add( new_edge )
+                new_edge = Edge.create_edge(related_node, node, group=group, metadata=md)
+            s.add(new_edge)
 
     @staticmethod
     def remove_all_edges(node):
-        node.session.query(Edge).filter(or_(Edge.left_id==node.id, Edge.right_id==node.id)).delete()
+        node.session.query(Edge).filter(or_(Edge.left_id == node.id, Edge.right_id == node.id)).delete()
 
 
-
-class Node(Base):
-    __tablename__ = "nodes"
+class AbstractNode(Base):
+    __abstract__ = True
     id = Column(Integer, primary_key=True)
     discriminator = Column(Unicode(50))
-    __mapper_args__ = {'polymorphic_on': discriminator, 'extension':Callback(), 'polymorphic_identity': u'node'}
-    name = Column(Unicode(255))
-    description = Column(UnicodeText)
-    node_key = Column(Unicode(100), unique=True)
+    _name = Column('name', Unicode(255))
+    _description = Column('description', UnicodeText)
+    _node_key = Column('node_key', Unicode(100), unique=True)
 
     # Dates
-    created_at = Column(DateTime(), default=datetime.datetime.now)
-    modified_at = Column(DateTime(), default=datetime.datetime.now)
-
-    # Users
-    created_by_id = Column(Integer)
-    modified_by_id = Column(Integer)
+    _created_at = Column('created_at', DateTime(), default=datetime.datetime.now)
+    _modified_at = Column('modified_at', DateTime(), default=datetime.datetime.now)
 
     def __init__(self, *args, **kw):
-        super(Node, self).__init__(*args, **kw)
+        super(AbstractNode, self).__init__(*args, **kw)
 
     def __unicode__(self):
         return self.name or ""
+
+
+    # def __getattribute__(self, name):
+    #     attr = '_{0}'.format(name)
+    #     if hasattr(self, attr):
+    #         return super(AbstractNode, self).__getattribute__(attr)
+    #     return super(AbstractNode, self).__getattribute__(name)
+
+    def __getattr__(self, name):
+        if hasattr(self, name) :
+            object.__getattr__(self, name)
+        else:
+            object.__getattr__(self, '_{0}'.format(name))
+
+
+    def __setattr__(self, name, value):
+        if hasattr(self, name) or name[:1] == '_':
+            object.__setattr__(self, name, value)
+        else:
+            object.__setattr__(self, '_{0}'.format(name), value)
 
     @property
     def classname(self):
@@ -208,20 +230,22 @@ class Node(Base):
         return object_session(self)
 
     def _get_children(self, discriminators=None, group=None, relation_type=None, order_by=None):
-        return self._get_related_node_query(Node, Edge.CHILD, discriminators, group, relation_type, order_by).all()
+        return self._get_related_node_query(self.__class__, Edge.CHILD, discriminators, group, relation_type, order_by).all()
+
     def _set_children(self, children=[], discriminators=None, group=None, relation_type=None, metadata=[]):
         Edge.update_child_edges(self, children, discriminators=discriminators, group=group, relation_type=relation_type, metadata=metadata)
 
     def _get_parents(self, discriminators=None, group=None, relation_type=None, order_by=None):
-        return self._get_related_node_query(Node, Edge.PARENT, discriminators, group, relation_type, order_by).all()
+        return self._get_related_node_query(self.__class__, Edge.PARENT, discriminators, group, relation_type, order_by).all()
+
     def _set_parents(self, parents=[], discriminators=None, group=None, relation_type=None, metadata=[]):
-        Edge.update_parent_edges( self, parents, discriminators=discriminators, group=group, relation_type=relation_type, metadata=metadata)
+        Edge.update_parent_edges(self, parents, discriminators=discriminators, group=group, relation_type=relation_type, metadata=metadata)
 
     def _get_child(self, discriminators=None, group=None, relation_type=False, order_by=None):
-        return self._get_related_node_query(Node, Edge.CHILD, discriminators, group, relation_type, order_by).first()
+        return self._get_related_node_query(self.__class__, Edge.CHILD, discriminators, group, relation_type, order_by).first()
 
     def _get_parent(self, discriminators=None, group=None, relation_type=False, order_by=None):
-        return self._get_related_node_query(Node, Edge.PARENT, discriminators, group, relation_type, order_by).first()
+        return self._get_related_node_query(self.__class__, Edge.PARENT, discriminators, group, relation_type, order_by).first()
 
     def _get_child_edges(self, discriminators=None, group=None, relation_type=None, order_by=None):
         return self._get_related_node_query(Edge, Edge.CHILD, discriminators, group, relation_type, order_by).all()
@@ -239,10 +263,10 @@ class Node(Base):
 
         query = self.session.query(query_cls)
         if query_cls == Edge:
-            query = query.select_from(Node)
+            query = query.select_from(self.__class__)
 
         clauses = self._get_related_node_query_clauses(relation, discriminators, group, relation_type)
-        node_edge = (Edge, Node.id==Edge.right_id) if relation==Edge.CHILD else (Edge, Node.id==Edge.left_id)
+        node_edge = (Edge, self.__class__.id == Edge.right_id) if relation == Edge.CHILD else (Edge, self.__class__.id == Edge.left_id)
         query = query.join(node_edge).filter(and_(*clauses))
         if order_by:
             if isinstance(order_by, list):
@@ -254,19 +278,19 @@ class Node(Base):
     def _get_related_node_query_clauses(self, relation=Edge.CHILD, discriminators=None, group=None, relation_type=None):
         """docstring for _get_related_node_query_clauses"""
         clauses = []
-        if relation==Edge.CHILD:
-            clauses.append(Edge.parent==self)
+        if relation == Edge.CHILD:
+            clauses.append(Edge.parent == self)
         else:
-            clauses.append(Edge.child==self)
+            clauses.append(Edge.child == self)
 
         if discriminators:
-            clauses.append(Node.discriminator.in_(discriminators))
+            clauses.append(self.__class__.discriminator.in_(discriminators))
 
-        if not group == False:
-            clauses.append(Edge.group_name==group)
+        if group is not False:
+            clauses.append(Edge._group_name == group)
 
-        if not relation_type == False:
-            clauses.append(Edge.relation_type==relation_type)
+        if relation_type is not False:
+            clauses.append(Edge._relation_type == relation_type)
 
         return clauses
 
@@ -328,15 +352,15 @@ class Children(object):
         self.relation_type = kws.get('relation_type', None)
 
     def __get__(self, instance, cls):
-        return instance._get_children( discriminators=self.discriminators, group=self.group_name, relation_type=self.relation_type )
+        return instance._get_children(discriminators=self.discriminators, group=self.group_name, relation_type=self.relation_type)
 
     def __set__(self, instance, value):
         discriminators = self.discriminators
         if discriminators:
             for child in value:
-                if not child.discriminator in discriminators:
+                if child.discriminator not in discriminators:
                     raise ValueError("One of the children passed as argument is of wrong type")
-        instance._set_children( value, discriminators=discriminators, group=self.group_name, relation_type=self.relation_type )
+        instance._set_children(value, discriminators=discriminators, group=self.group_name, relation_type=self.relation_type)
 
     @property
     def discriminators(self):
@@ -353,21 +377,20 @@ class Parents(object):
         self.relation_type = kws.get('relation_type', None)
 
     def __get__(self, instance, cls):
-        return instance._get_parents( discriminators=self.discriminators, group=self.group_name, relation_type=self.relation_type )
+        return instance._get_parents(discriminators=self.discriminators, group=self.group_name, relation_type=self.relation_type)
 
     def __set__(self, instance, value):
         discriminators = self.discriminators
         if discriminators:
             for parent in value:
-                if not parent.discriminator in discriminators:
+                if parent.discriminator not in discriminators:
                     raise ValueError("One of the parents passed as argument is of wrong type")
-        instance._set_parents( value, discriminators=discriminators, group=self.group_name, relation_type=self.relation_type )
+        instance._set_parents(value, discriminators=discriminators, group=self.group_name, relation_type=self.relation_type)
 
     @property
     def discriminators(self):
         return get_discriminators(*self.classes, include_subclasses=self.include_subclasses)
 
 
-Node.children = Children(Node)
-Node.parents = Parents(Node)
-
+AbstractNode.children = Children(AbstractNode)
+AbstractNode.parents = Parents(AbstractNode)
--- node/util.py
+++ node/util.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
+import sys
+import json
+from sqlalchemy import UnicodeText
+from sqlalchemy.orm import EXT_CONTINUE
+from sqlalchemy.orm.interfaces import MapperExtension
+from sqlalchemy.types import TypeDecorator
 
 class ConstantMeta(type):
     """Collects unicode constants set on class"""
     def __init__(cls, class_name, bases, class_dict):
         cls.values = []
         cls.keys = {}
-        for k,v in class_dict.iteritems():
+        for k, v in class_dict.iteritems():
             if type(v) == unicode or type(v) == int:
                 cls.values.append(v)
                 cls.keys[v] = k
@@ -17,7 +23,6 @@ class ConstantMeta(type):
 #         for k,v in class_dict.iteritems():
 #             if type(v) == unicode:
 #                 cls.values.append(v)
-
 
 def get_subclasses(cls):
     subclasses = cls.__subclasses__()
@@ -43,3 +48,81 @@ def get_discriminators(*args, **kws):
     if len(items) > 0:
         return items
     return None
+
+InputMismatchError = TypeError("Inputs must be both unicode or both bytes")
+
+# Always takes the same time comparing a & b
+# regardless of similarity. Prevents learning
+# when brute forcing password.
+def constant_time_compare(self, a, b):
+    if isinstance(a, unicode):
+        if not isinstance(b, unicode):
+            raise InputMismatchError
+        is_py3_bytes = False
+    elif isinstance(a, bytes):
+        if not isinstance(b, bytes):
+            raise InputMismatchError
+        is_py3_bytes = sys.version_info >= (3, 0)
+    else:
+        raise InputMismatchError
+
+    result = 0
+    if is_py3_bytes:
+        for x, y in zip(a, b):
+            result |= x ^ y
+    else:
+        for x, y in zip(a, b):
+            result |= ord(x) ^ ord(y)
+    return result == 0
+
+class Callback(MapperExtension):
+    """ Extention to add pre-commit hooks.
+
+    __mapper_args__ = {'polymorphic_on': discriminator, 'extension':Callback(), 'polymorphic_identity': u'node'}
+
+
+    Hooks will be called in Mapped classes if they define any of these
+    methods:
+      * _pre_insert()
+      * _post_insert()
+      * _pre_delete()
+      * _pre_update()
+    """
+    def before_insert(self, mapper, connection, instance):
+        f = getattr(instance, "_pre_insert", None)
+        if f:
+            f()
+        return EXT_CONTINUE
+
+    def after_insert(self, mapper, connection, instance):
+        f = getattr(instance, "_post_insert", None)
+        if f:
+            f()
+        return EXT_CONTINUE
+
+    def before_delete(self, mapper, connection, instance):
+        f = getattr(instance, "_pre_delete", None)
+        if f:
+            f()
+        return EXT_CONTINUE
+
+    def before_update(self, mapper, connection, instance):
+        f = getattr(instance, "_pre_update", None)
+        if f:
+            f()
+        return EXT_CONTINUE
+
+
+class JSONEncodedObj(TypeDecorator):
+    """Represents an immutable structure as a json-encoded string."""
+    impl = UnicodeText
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value, use_decimal=True)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value, use_decimal=True)
+        return value
